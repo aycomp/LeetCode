3cc1b86f05ef6fcc9b9a81b2e1bcd7431935748d
ayse <aozdemir2011@gmail.com>
2024-01-07 11:55:09 +0300

    Update 1894-merge-strings-alternately.java

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -2,18 +2,11 @@ class Solution {
     public String mergeAlternately(String word1, String word2) {
         int w1 = word1.length();
         int w2 = word2.length();
+        int biggest = w1 > w2 ? w1 : w2;
         StringBuilder result = new StringBuilder();
-        int biggest = (w1-w2) > 0 ? w1 : w2;
-
-        for(int i=0; i<biggest; i++) {
-            if(i < word1.length()){
-                result.append(word1.charAt(i));
-            }
-            
-            if(i < word2.length()){
-                result.append(word2.charAt(i));
-            }
+        for(int i=0; i<biggest; i++){
+            result.append(w1 > i ? word1.charAt(i) : "");
+            result.append(w2 > i ? word2.charAt(i) : "");
         }
-       return result.toString(); 
+        return result.toString();
     }
-}
