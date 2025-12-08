187a83353b3f8b35d1e87f47de4fec80edad48c0
ayse <aozdemir2011@gmail.com>
2025-12-08 11:52:27 +0300

    Time: 60 ms (9.24%), Space: 70.8 MB (8.19%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,11 +1,10 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-
         Map<Integer, Integer> map = new HashMap<>();
         int left = 0;
         int maxLen = 0;
 
-        for (int right = 0; right < fruits.length; right ++) {
+        for (int right = 0; right < fruits.length; right++) {
             map.put(fruits[right], map.getOrDefault(fruits[right], 0) + 1);
 
             while (map.size() > 2) {
@@ -14,13 +13,12 @@ class Solution {
                 if (map.get(fruits[left]) == 0) {
                     map.remove(fruits[left]);
                 }
-
                 left++;
             }
+
             maxLen = Math.max(maxLen, right - left + 1);
         }
 
         return maxLen;
-        
     }
 }
