e5619ba11409d91186abea6e949bbc3241a8ff01
ayse <aozdemir2011@gmail.com>
2025-12-22 13:42:02 +0300

    Time: 14 ms (82.47%), Space: 93.5 MB (32.11%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -3,12 +3,14 @@ class Solution {
         Set<Integer> set = new HashSet<>();
 
         for (int num : nums) {
-            if(set.contains(num)) {
+            if (!set.contains(num)) {
+                set.add(num);
+            } else {
                 return true;
             }
-            set.add(num);
         }
 
         return false;
+        
     }
 }
